eb855f321f9badc7a8cf638eee7b0d174cec67fb
AbelT <abelteame22@gmail.com>
2022-08-16 01:25:44 -0700

    more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * reverse_listint - reverse the list
+ * @head: the list
+ *
+ * Return: the list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t temp = *head;
+	return (*head);
+}
