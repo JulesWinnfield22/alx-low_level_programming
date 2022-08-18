c7f4ff2bf674c789f19285962f0686581a6c4521
AbelT <abelteame22@gmail.com>
2022-08-18 07:08:26 -0700

    bit_manipulation

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN
 #define MAIN
 typedef struct bit bit_s;
+typedef struct bit_ptr bit_p;
+
+struct bit_ptr {
+	int *b;
+	struct bit_ptr next;
+};
 
 struct bit {
 	int b;
@@ -11,4 +17,5 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif /* MAIN */
